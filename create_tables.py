c864baa8d481f9edf4cef28cc4922f8beda0facf
Lu <lu.maltsis@gmail.com>
2022-02-17 11:19:54 +0100

    added guilds to table managment

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,6 @@
-from lib.database import User, Timer, Allow
+from lib.database import User, Timer, Allow, Guild
 
 User.create_table()
 Timer.create_table()
 Allow.create_table()
+Guild.create_table()
--- delete_tables.py
+++ delete_tables.py
@@ -1,5 +1,6 @@
-from lib.database import User, Timer, Allow
+from lib.database import User, Timer, Allow, Guild
 
 User.delete_table()
 Timer.delete_table()
 Allow.delete_table()
+Guild.delete_table()
